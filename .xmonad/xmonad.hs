80dd41f84e2eb7df201cd290a669e302f99af001
SFR-git <664ecb67-c3b4-4f09-854a-f4535569b492@sfr.anonaddy.com>
2020-06-27 19:51:46 +0300

    Lots of changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,14 +5,11 @@
 --  ____) | |    | | \ \  || ko-fi.com/supportsfr
 -- |_____/|_|    |_|  \_\ || /u/sfrvtma
 
--- Imports
-    -- Base
 import XMonad
 import System.IO (hPutStrLn)
 import System.Exit (exitSuccess)
 import qualified XMonad.StackSet as W
-
-    -- Actions
+import XMonad.ManageHook
 import XMonad.Actions.CopyWindow (kill1, killAllOtherCopies)
 import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
 import XMonad.Actions.MouseResize
@@ -20,22 +17,17 @@ import XMonad.Actions.Promote
 import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
 import XMonad.Actions.WindowGo (runOrRaise)
 import XMonad.Actions.WithAll (sinkAll, killAll)
-
-    -- Data
 import Data.Char (isSpace)
 import Data.List
 import Data.Monoid
 import Data.Maybe (isJust)
 import qualified Data.Map as M
-
-    -- Hooks
 import XMonad.Hooks.EwmhDesktops 
-import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
-import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ServerMode
 import XMonad.Hooks.SetWMName
-
-    -- Layout
+import XMonad.Hooks.DynamicLog
 import XMonad.Layout.SimplestFloat
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.LayoutModifier
@@ -46,148 +38,123 @@ import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBO
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Renamed (renamed, Rename(Replace))
 import XMonad.Layout.Spacing
+import XMonad.Layout.GridVariants (Grid(Grid))
 import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
 import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
-
-    -- Util
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
 import XMonad.Util.SpawnOnce
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
 
--- Variables
-myModMask :: KeyMask
-myModMask = mod4Mask
-
-myTerminal :: String
-myTerminal = "alacritty"
-
-myBorderWidth :: Dimension
-myBorderWidth = 2
-
-myNormColor :: String
-myNormColor = "#1a1a1a"
-
-myFocusColor :: String
-myFocusColor = "#fafafa"
-
-altMask :: KeyMask
-altMask = mod1Mask
-
-windowCount :: X (Maybe String)
-windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
-
--- Autostart
-myStartupHook :: X ()
-myStartupHook = do
-          spawnOnce "feh --randomize --bg-fill /usr/share/wallpapers/* &" 
-          spawnOnce "picom &"
-          spawnOnce "udiskie -Nt &"
-          spawnOnce "nm-applet &"
-          spawnOnce "volumeicon &"
-          spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x292d3e --height 18 &"
-          spawnOnce "setxkbmap gb,il &"
-          spawnOnce "sleep 1; xmodmap ~/.Xmodmap &"
-          setWMName "LG3D"
-
--- Keybinds
-myKeys :: [(String, X ())]
-myKeys =
-    -- xmonad
-        [ ("M-C-r", spawn "xmonad --recompile")                        -- Recompiles xmonad
-        , ("M-S-r", spawn "xmonad --restart")                          -- Restarts xmonad
-        , ("M-S-q", io exitSuccess)                                    -- Quits xmonad
-    
-    -- Window management
-        , ("M-S-c", kill1)                                             -- Kill the currently focused client
-        , ("M-S-a", killAll)                                           -- Kill all windows on current workspace
-        , ("M-m", windows W.focusMaster)                               -- Move focus to the master window
-        , ("M-j", windows W.focusDown)                                 -- Move focus to the next window
-        , ("M-k", windows W.focusUp)                                   -- Move focus to the previous window
-        , ("M-S-j", windows W.swapDown)                                -- Swap focused window with next window
-        , ("M-S-k", windows W.swapUp)                                  -- Swap focused window with previous window
-        , ("M-f", sendMessage (T.Toggle "floats"))                     -- Make window float
-        , ("M-`", withFocused $ windows . W.sink)                      -- Push floating window back to tile
-        , ("M-S-`", sinkAll)                                           -- Push all floating windows to tile
-        , ("M-h", sendMessage Shrink)                                  -- Shrink horizontal window width
-        , ("M-l", sendMessage Expand)                                  -- Expand horizontal window width
-        , ("M-C-j", sendMessage MirrorShrink)                          -- Shrink vertical window width
-        , ("M-C-k", sendMessage MirrorExpand)                          -- Expand vertical window width
-
-    -- Miscellaneous
-        , ("M-<Return>", spawn (myTerminal))                           -- Open Terminal
-        , ("<F13>", spawn "~/.config/rofi/launcher/launcher.sh")       -- Application Launcher
-        , ("<F15>", spawn "xkb-switch -n && xmodmap ~/.Xmodmap")       -- Cycle keyboard layouts
-
-    -- Layouts
-        , ("M-<Tab>", sendMessage NextLayout)                -- Switch to next layout
-        , ("M-C-M1-<Up>", sendMessage Arrange)
-        , ("M-C-M1-<Down>", sendMessage DeArrange)
-        , ("M-<Space>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts) -- Toggles noborder/full
-       , ("M-S-<Space>", sendMessage ToggleStruts)         -- Toggles struts
-       , ("M-S-n", sendMessage $ MT.Toggle NOBORDERS)      -- Toggles noborder
-       , ("M-<KP_Multiply>", sendMessage (IncMasterN 1))   -- Increase number of clients in master pane
-       , ("M-<KP_Divide>", sendMessage (IncMasterN (-1)))  -- Decrease number of clients in master pane
-       , ("M-S-<KP_Multiply>", increaseLimit)              -- Increase number of windows
-       , ("M-S-<KP_Divide>", decreaseLimit)                -- Decrease number of windows
-       ]
-                     
--- Workspaces
-  where
-        doubleLts '<' = "<<"
-        doubleLts x   = [x]
-        
-myWorkspaces :: [String]   
-myWorkspaces = clickable
-               $ ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  where                                                                      
-        clickable l = [ "<action=xdotool key super+" ++ show n ++ ">" ++ ws ++ "</action>" |
-                      (i,ws) <- zip [1..9] l,                                        
-                      let n = i ] 
-
-myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
-myManageHook = composeAll
-     [ title =? "Oracle VM VirtualBox Manager"     --> doFloat
-     , className =? "Oracle VM VirtualBox Manager" --> doShift  ( myWorkspaces !! 6)
-     , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat  -- Float Firefox Dialog
-     ] 
-
--- Layouts
+-- Layouts (NOT DONE)
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
-tall     = renamed [Replace "tall"]
+tall     = renamed [Replace "Master & Stacked"]
            $ limitWindows 12
            $ mySpacing 4
            $ ResizableTall 1 (3/100) (1/2) []
-floats   = renamed [Replace "floats"]
+monocle  = renamed [Replace "Monocle"]
+           $ mySpacing 4
+           $ limitWindows 20 Full
+grid     = renamed [Replace "Grid"]
+           $ limitWindows 12
+           $ mySpacing 4
+           $ mkToggle (single MIRROR)
+           $ Grid (16/10)
+wide    = renamed [Replace "Vertical M&S"]
+           $ limitWindows 12
+           $ mySpacing 4
+           $ Mirror
+           $ ResizableTall 1 (3/100) (1/2) []
+floats   = renamed [Replace "Floating"]
            $ limitWindows 20 simplestFloat
           
-    -- The layout hook
-myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats $
-               mkToggle (NBFULL ?? NOBORDERS ?? EOT) myDefaultLayout
-             where
-               -- I've commented out the layouts I don't use.
-               myDefaultLayout =     smartBorders tall
-                                 ||| floats
-
 -- Main
 main :: IO ()
 main = do
-    xmproc <- spawnPipe "polybar bar1" -- Launch Polybar
-    xmonad $ ewmh def
-        { manageHook = ( isFullscreen --> doFullFloat ) <+> myManageHook <+> manageDocks
-        , handleEventHook    = serverModeEventHookCmd 
-                               <+> serverModeEventHook 
-                               <+> serverModeEventHookF "XMONAD_PRINT" (io . putStrLn)
-                               <+> docksEventHook
-        , modMask            = myModMask
-        , terminal           = myTerminal
-        , startupHook        = myStartupHook
-        , layoutHook         = myLayoutHook 
-        , workspaces         = myWorkspaces
-        , borderWidth        = myBorderWidth
-        , normalBorderColor  = myNormColor
-        , focusedBorderColor = myFocusColor                         
-        } `additionalKeysP` myKeys 
+    dbus <- D.connectSession
+    D.requestName dbus (D.busName_ "org.xmonad.Log")
+        [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
 
+    xmonad $ ewmh $ docks $ def
+        { modMask            = mod4Mask
+        , terminal           = "alacritty"
+
+    -- Autostart
+        , startupHook = do
+            spawnOnce "feh --randomize --bg-fill /usr/share/wallpapers/* &" 
+            spawnOnce "picom &"
+            spawnOnce "udiskie -Nt &"
+            spawnOnce "polybar bar1 &"
+            spawnOnce "setxkbmap gb,il &"
+            spawnOnce "sleep 1; xmodmap ~/.Xmodmap &"
+            setWMName "LG3D"
+
+    -- Layouts (NOT DONE)
+        , layoutHook = 
+            avoidStruts $ mouseResize $ smartBorders $ windowArrange
+                tall
+            ||| monocle
+            ||| wide
+            ||| grid
+            ||| floats
+
+    -- Workspaces
+        , workspaces         = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    -- Border
+        , normalBorderColor  = "#1a1a1a"
+        , focusedBorderColor = "#fafafa"
+
+    -- Polybar
+        , logHook = dynamicLogWithPP (def
+            { ppOutput = dbusOutput dbus
+            , ppCurrent = wrap ("%{F" ++ "#fafafa" ++ "} ") " %{F-}"
+            , ppVisible = wrap ("%{F" ++ "#fafafa" ++ "} ") " %{F-}"
+            , ppHidden = wrap ("%{F" ++ "#545454" ++ "} ") " %{F-}"
+            , ppSep = " | "
+            , ppWsSep = ""
+            })
+
+    -- Keybinds
+        } `additionalKeysP` 
+            [ ("M-C-r", spawn "xmonad --recompile")                        -- Recompiles xmonad
+            , ("M-S-r", spawn "xmonad --restart")                          -- Restarts xmonad
+            , ("M-S-q", io exitSuccess)                                    -- Quits xmonad
+            , ("M-<Return>", spawn "alacritty")                            -- Open Alacritty
+            , ("M-S-c", kill1)                                             -- Kill the currently focused client
+            , ("M-S-a", killAll)                                           -- Kill all windows on current workspace
+            , ("M-m", windows W.focusMaster)                               -- Move focus to the master window
+            , ("M-j", windows W.focusDown)                                 -- Move focus to the next window
+            , ("M-k", windows W.focusUp)                                   -- Move focus to the previous window
+            , ("M-S-j", windows W.swapDown)                                -- Swap focused window with next window
+            , ("M-S-k", windows W.swapUp)                                  -- Swap focused window with previous window
+            , ("M-f", sendMessage (T.Toggle "floats"))                     -- Make window float
+            , ("M-`", withFocused $ windows . W.sink)                      -- Push floating window back to tile
+            , ("M-S-`", sinkAll)                                           -- Push all floating windows to tile
+            , ("M-h", sendMessage Shrink)                                  -- Shrink horizontal window width
+            , ("M-l", sendMessage Expand)                                  -- Expand horizontal window width
+            , ("M-C-j", sendMessage MirrorShrink)                          -- Shrink vertical window width
+            , ("M-C-k", sendMessage MirrorExpand)                          -- Expand vertical window width
+            , ("M-<Tab>", sendMessage NextLayout)                          -- Switch to next layout
+            , ("<F13>", spawn "~/.config/rofi/launcher/launcher.sh")       -- Application Launcher
+            , ("<F14>", spawn "scrot ~/Pictures/Screenshots/%b%d-%H%M%S.png") -- Take a fullscreen screenshot
+            , ("S-<F14>", spawn "sleep 0.2; scrot -s ~/Pictures/Screenshots/%b%d-%H%M%S.png") -- Take screenshot of area
+            , ("<F15>", spawn "xkb-switch -n && xmodmap ~/.Xmodmap")       -- Cycle keyboard layouts
+            ] 
+
+-- (NOT DONE)
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal objectPath interfaceName memberName) {
+            D.signalBody = [D.toVariant $ UTF8.decodeString str]
+        }
+    D.emit dbus signal
+  where
+    objectPath = D.objectPath_ "/org/xmonad/Log"
+    interfaceName = D.interfaceName_ "org.xmonad.Log"
+    memberName = D.memberName_ "Update"
