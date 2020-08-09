21b96defbe8ecb0cce4143737a0cce2fc08a3ecc
SFR-git <664ecb67-c3b4-4f09-854a-f4535569b492@sfr.anonaddy.com>
2020-08-09 21:14:39 +0300

    Remove rofi powermenu

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -119,7 +119,7 @@ main = do
             , ("M-C-j", sendMessage MirrorShrink)                           -- Shrink vertical window width
             , ("M-C-k", sendMessage MirrorExpand)                           -- Expand vertical window width
             , ("M-<Tab>", sendMessage NextLayout)                           -- Switch to next layout
-            , ("M-S-<Return>", spawn "~/.config/rofi/launcher/launcher.sh") -- Application Launcher
+            , ("M-S-<Return>", spawn "~/.config/rofi/launcher.sh") -- Application Launcher
             , ("<F14>", spawn "scrot ~/Pictures/Screenshots/%y-%m-%d-%H%M%S.png") -- Take a fullscreen screenshot
             , ("S-<F14>", spawn "sleep 0.2; scrot -s ~/Pictures/Screenshots/%y-%m-%d-%H%M%S.png") -- Take screenshot of area
             , ("<F15>", spawn "xkb-switch -n && xmodmap ~/.Xmodmap")        -- Cycle keyboard layouts
